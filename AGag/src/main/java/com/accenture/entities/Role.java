e5f37ad45a4a73efd0d92d27ea677ef983d98ea9
Aleksandar Simeonov <troqta@gmail.com>
2019-01-26 19:53:06 +0200

    Fixed roles, added favico

--- AGag/src/main/java/com/accenture/entities/Role.java
+++ AGag/src/main/java/com/accenture/entities/Role.java
@@ -40,7 +40,7 @@ public class Role implements GrantedAuthority {
 
 	@Override
 	public String getAuthority() {
-		return null;
+		return authority;
 	}
 
 	@Override
--- AGag/src/main/java/com/accenture/utils/Util.java
+++ AGag/src/main/java/com/accenture/utils/Util.java
@@ -4,7 +4,7 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class Util {
-    public static final String DEFAULT_PIC_PATH = "C:\\Users\\aleksandar.simeonov\\Desktop\\default.png";
+    public static final String DEFAULT_PIC_PATH = "C:\\Users\\Fast1r1s\\Desktop\\default.png";
     public static final String PASSWORD_LENGTH_MESSAGE = "Password length should be between 5 and 20 symbols long";
     public static final String USERNAME_LENGTH_MESSAGE = "Username length should be between 5 and 20 symbols long";
 
